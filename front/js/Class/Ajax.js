28c098c192d80c3b0d4be4f0e4540550763f920f
Amaury Le Meur <amaury.lemeur@gmail.com>
2021-05-05 17:13:36 +0200

    Retour en arrière AJAX

--- front/js/Class/Ajax.js
+++ front/js/Class/Ajax.js
@@ -4,6 +4,7 @@ class Ajax {
 			var request = new XMLHttpRequest();
 			request.onreadystatechange = function() {
 				if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+					var response = JSON.parse(this.responseText);
 					resolve(response);
 				}
 			};
--- front/js/index.js
+++ front/js/index.js
@@ -1,6 +1,5 @@
 const ajax = new Ajax;
 const product = new Index;
 ajax.request("GET", "http://localhost:3000/api/teddies").then(response => {
-	var response = JSON.parse(this.responseText);
 	product.displayItems(response);
 });
