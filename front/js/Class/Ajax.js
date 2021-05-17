22dfbb5d142b224845fe509beecd8a4c9f233b36
Amaury Le Meur <amaury.lemeur@gmail.com>
2021-05-17 12:12:12 +0200

    Dernière modification class Ajax

--- front/js/Class/Ajax.js
+++ front/js/Class/Ajax.js
@@ -1,19 +1,4 @@
 class Ajax {
-	/*request (method, url, data=null) {
-		return new Promise((resolve, reject) => {
-			var request = new XMLHttpRequest();
-			request.onreadystatechange = function() {
-				if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-					var response = JSON.parse(this.responseText);
-					resolve(response);
-				}
-			};
-			request.open(method, url);
-			request.setRequestHeader('Content-Type', 'application/json');
-			request.setRequestHeader('Accept', 'application/json');
-			request.send(data);
-		});
-	}*/
 	async postRequest(url = '', data = null) {
 	  // Default options are marked with *
 	  const response = await fetch(url, {
